529b4f0929fe00069a4bcc6ebecab4ca75a12428
Andrew Lamoureux <andrew@vector35.com>
2019-11-01 15:35:12 -0400

    Add ppcd disassembler (really this time)

---
+++ ppcd_main.cpp
@@ -0,0 +1,31 @@
+#include <string.h>
+#include <stdint.h>
+
+#include "ppcd.h"
+
+static uint32_t bswap(uint32_t data)
+{
+    return ((data >> 24)             ) |
+           ((data >>  8) & 0x0000ff00) |
+           ((data <<  8) & 0x00ff0000) |
+           ((data << 24)             );
+}
+
+extern "C" int disassemble(uint32_t addr, uint8_t *data, int len, char *result)
+{
+	PPCD_CB disa;
+
+	disa.pc = addr;
+	disa.instr = bswap(*(uint32_t *)data);
+	PPCDisasm(&disa);
+
+	if(disa.iclass == PPC_DISA_ILLEGAL)
+		return 0;
+
+	strcpy(result, disa.mnemonic);
+	if(disa.operands[0]) {
+		strcat(result, " ");
+		strcat(result, disa.operands);
+	}
+	return 4;
+}
